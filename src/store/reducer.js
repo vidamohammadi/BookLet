4dea384e72da6ac760508d599a808166eccfffe6
vida <m.vidamohammadi@gmail.com>
2022-01-16 18:49:49 +0330

    reducer for slider two

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 const inistialState = {
-    sliderOneId: 0
+    sliderOneId: 0,
+    sliderTwoId:0
 }
 
 const reducer = (state = inistialState, action) => {
@@ -7,26 +8,36 @@ const reducer = (state = inistialState, action) => {
         case 'NEXT':
             if (state.sliderOneId === 3) {
                 return {
+                    ...state,
                     sliderOneId: 0
                 }
             } else {
                 return {
+                    ...state,
                     sliderOneId: state.sliderOneId + 1
                 }
             }
         case 'PREV':
             if (state.sliderOneId === 0) {
                 return {
+                    ...state,
                     sliderOneId: 3
                 }
             } else {
                 return {
+                    ...state,
                     sliderOneId: state.sliderOneId - 1
                 }
             }
         case 'CLICK':
             return{
+                ...state,
                 sliderOneId: action.id - 1
+            }
+        case 'CLICKTWO':
+            return{
+                ...state,
+                sliderTwoId: action.id - 1
             }
         default:
             return state
